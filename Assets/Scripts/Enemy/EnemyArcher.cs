09aae6acc38543a2afbaba19665d5f105014abed
ItsMaTime <151616910+ItsMaTime@users.noreply.github.com>
2025-11-29 16:30:59 +0100

    arrow shooting player

--- Assets/Scripts/Enemy/EnemyArcher.cs
+++ Assets/Scripts/Enemy/EnemyArcher.cs
@@ -1,23 +1,49 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyArcher : MonoBehaviour
 {
     public GameObject arrow;
-    public Transform arrowPosition;
     public Transform arrowSpawnPosition;
+    public float shootingDistance;
+    private float timer;
+    public float shootInterval;
+    public float speed;
+
+    private GameObject player;
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject spawnedArrow = Instantiate(arrow, arrowSpawnPosition.position, Quaternion.identity);
-        Rigidbody2D arrowRigidbody = spawnedArrow.GetComponent<Rigidbody2D>();
-        arrowRigidbody.linearVelocity = new Vector2(20, 10);
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
+    //if player is in trigger (triggerenter) -> shoot (above or instantiate), else -> don't shoot
     void Update()
     {
-        
+        Vector2 direction = player.transform.position - arrowSpawnPosition.transform.position;
+        if (direction.magnitude <= shootingDistance)
+        {
+            
+            timer += Time.deltaTime;
+            if (timer > shootInterval)
+            {
+                timer = 0;
+                Shoot();
+            } 
+        }
     }
+
+    void Shoot()
+    {
+        GameObject spawnedArrow = Instantiate(arrow, arrowSpawnPosition.position, Quaternion.identity); //Quaternion = rotation
+        Rigidbody2D arrowRigidbody = spawnedArrow.GetComponent<Rigidbody2D>();
+        Vector2 direction = player.transform.position - arrowSpawnPosition.transform.position;
+        arrowRigidbody.linearVelocity = direction.normalized * speed;
+    }
+    
+    
 }
