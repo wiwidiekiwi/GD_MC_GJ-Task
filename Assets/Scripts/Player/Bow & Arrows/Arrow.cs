9b930afc464315056d69c312139e473378caa205
wiwidiekiwi <stanislawskiwojciech47@gmail.com>
2025-11-30 19:18:01 +0100

    Bow & Arrow basics
    basics = player has dummy bow, can shoot dummy arrows and can see how the arrows will fly
    (This needs to be reviewed by a proper engineer)

---
+++ Assets/Scripts/Player/Bow & Arrows/Arrow.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool hasHit;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasHit == false)
+        {
+            float angle = Mathf.Atan2(rb.linearVelocity.y, rb.linearVelocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        hasHit = true;
+        rb.linearVelocity = Vector2.zero;
+    }
+}
